386efc94d7c1f75f8bbc4bfa159d5044c05f0426
nhiyen23 <takenbyyn2356@gmail.com>
2026-02-10 08:45:07 +0700

    Update hom_nay_an_gi.py

--- hom_nay_an_gi.py
+++ hom_nay_an_gi.py
@@ -73,7 +73,7 @@ st.markdown("""
         font-size: 1.1rem;
         color: #777;
         max-width: 800px;
-        margin: 0 auto 2rem;
+        # margin: 0 auto 2rem;
         line-height: 1.8;
         text-align: center !important;
     }
