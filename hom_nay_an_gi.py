f98d562805629ca8046d0782f14c70870a879608
nhiyen23 <takenbyyn2356@gmail.com>
2026-02-10 15:23:31 +0700

    Update hom_nay_an_gi.py

--- hom_nay_an_gi.py
+++ hom_nay_an_gi.py
@@ -150,7 +150,7 @@ st.markdown("""
     
     .feature-card {
         background: white;
-        padding: 18px 20px;
+        padding: 16px 18px;
         border-radius: 20px;
         box-shadow: 0 5px 20px rgba(0,0,0,0.08);
         transition: all 0.3s ease;
